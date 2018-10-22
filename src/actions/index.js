681f334e8e1551f99011d0a848a1fc683d1847cd
ralph kemp <ralphk3mp@gmail.com>
2018-10-23 00:54:40 +0100

    mapped state to props in cardslist as a test, and seems to be working fine. Going to plan out design of card or cards by hand to have a good reference to what im creating.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 import { FETCH_CHARITY } from './types';
 
-export const fetchCharity
+export const fetchCharity = () => async dispatch => {
+  const res = await axios.get(`https://api.justgiving.com/fad4af22/v1/charity/183560`);
+  dispatch({ type: FETCH_CHARITY, payload: res.data});
+}
