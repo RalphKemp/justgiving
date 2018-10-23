db337baf945ab2c7ab601bfe2d883ff8bb12b5d3
ralph kemp <ralphk3mp@gmail.com>
2018-10-23 17:35:20 +0100

    created a new inner container in the landing component to have more spacing control over items. Background colors for components are for temporary guidance

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,23 +1,34 @@
-import React from "react";
-import Card from "./Card";
-import Header from "./Header";
-import styled from "styled-components";
+import React from 'react';
+import Card from './Card';
+import Header from './Header';
+import styled from 'styled-components';
 
 const LandingContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background: linear-gradient(#ffffff, #d8c1da);
   display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ContentContainer = styled.div`
+  background-color: pink;
+  display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 70%;
+  height: 70%;
 `;
 
 const Landing = () => {
   return (
     <LandingContainer>
-      <Header />
-      <Card />
+      <ContentContainer>
+        <Header />
+        <Card />
+      </ContentContainer>
     </LandingContainer>
   );
 };
