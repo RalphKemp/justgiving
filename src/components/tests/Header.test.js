9a564d34f82b3c56f5792735fe140091e31d0e8c
ralph kemp <ralphk3mp@gmail.com>
2018-10-29 23:56:18 +0000

    created basic render test for Header, will implement in other tests too.

--- src/components/tests/Header.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import Header from '../Header.js';
-
-
-test('thing to work', () => {
-  const component = shallow(<Header />);
-  expect(component.exists('img')).to.equal(true);
-})
