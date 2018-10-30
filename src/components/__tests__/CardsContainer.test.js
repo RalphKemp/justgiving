f8583360cecdf403c6fcbbf94938465298a0ae63
ralph kemp <ralphk3mp@gmail.com>
2018-10-30 03:05:07 +0000

    all basic tests working but still trying to check props rendering, - the test has to be run after state is changed, so trying to figure that out.

--- src/components/__tests__/CardsContainer.test.js
+++ src/components/__tests__/CardsContainer.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mock } from 'enzyme';
 // using curly braces to import component without state.
-import { CardsContainer } from '../CardsContainer.js';
+import ConnectedCardsContainer, { CardsContainer } from '../CardsContainer.js';
+import configureStore from 'redux-mock-store';
 
 describe('CardsContainer', () => {
+  const mockStore = configureStore();
+
   it('renders correctly', () => {
     const wrapper = shallow(<CardsContainer />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  // it('renders props', async () => {
+  //   const store = mockStore({});
+  //   const wrapper = shallow(<ConnectedCardsContainer store={store} />).props();
+  //   console.log(wrapper);
+  // });
 });
